bbf2f7840fd44956d44f623c3ec9428894be46cd
volha010892 <66121147+volha010892@users.noreply.github.com>
2021-02-19 00:06:22 +0300

    feat: add array images

---
+++ src/Components/ImageArray.jsx
@@ -0,0 +1,43 @@
+import cat1 from '../assets/img/cat1.png';
+import cat2 from '../assets/img/cat2.png';
+import cat3 from '../assets/img/cat3.png';
+import cat4 from '../assets/img/cat4.png';
+import cat5 from '../assets/img/cat5.png';
+import cat6 from '../assets/img/cat6.png';
+import dog1 from '../assets/img/dog1.png';
+import dog2 from '../assets/img/dog2.png';
+import dog3 from '../assets/img/dog3.png';
+import dog4 from '../assets/img/dog4.png';
+import dog5 from '../assets/img/dog5.png';
+import dog6 from '../assets/img/dog6.png';
+
+export const cardsCat = [
+  { img: cat1, id: '1' },
+  { img: cat2, id: '2' },
+  { img: cat3, id: '3' },
+  { img: cat4, id: '4' },
+  { img: cat5, id: '5' },
+  { img: cat6, id: '6' },
+];
+export const cardsDog = [
+  { img: dog1, id: '1' },
+  { img: dog2, id: '2' },
+  { img: dog3, id: '3' },
+  { img: dog4, id: '4' },
+  { img: dog5, id: '5' },
+  { img: dog6, id: '6' },
+];
+export const catDog = [
+  { img: cat1, id: '1' },
+  { img: cat2, id: '2' },
+  { img: cat3, id: '3' },
+  { img: cat4, id: '4' },
+  { img: cat5, id: '5' },
+  { img: cat6, id: '6' },
+  { img: dog1, id: '1' },
+  { img: dog2, id: '2' },
+  { img: dog3, id: '3' },
+  { img: dog4, id: '4' },
+  { img: dog5, id: '5' },
+  { img: dog6, id: '6' },
+];
