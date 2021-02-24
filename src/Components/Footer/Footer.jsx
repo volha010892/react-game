9a64b09c03361f5611817701e373e30ae86e6539
volha010892 <66121147+volha010892@users.noreply.github.com>
2021-02-24 21:01:06 +0300

    feat: add footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import icon from '../../assets/img/icon.png';
+import rsSchool from '../../assets/img/rs_school_js.svg';
+import {FooterStyle} from './Styled/Footer';
+export default function Footer() {
+  return (
+    <FooterStyle>
+          Made by Volha Khmyl
+          <div onClick={()=>window.location ='https://github.com/volha010892'}>
+            <img src={icon} alt="git hub logo"/>
+          </div>
+          <div onClick={()=>window.location ="https://rs.school/js/"}>
+            <img src={rsSchool} alt="rsschool log" />
+          </div>
+     </FooterStyle>
+  );
+}
---
+++ src/Components/Footer/Styled/Footer.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+export const FooterStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  div {
+    cursor: pointer;
+    margin: 0 1em 0 1em;
+  }
+  div img {
+    height: 1.2em;
+  }
+`;
